3479eb43c04307fb37807a19b2c4dc3ccf4cbe20
Ola Zytek <zyteka@mit.edu>
2024-08-21 09:55:54 -0400

    Separating out training and eval datasets

--- full-experiment/examples.py
+++ full-experiment/examples.py
@@ -21,3 +21,18 @@ def load_examples(json_file):
     for convo in training_data:
         examples.append(create_example(convo))
     return examples
+
+
+def get_data(json_file, split=0.6):
+    all_data = load_examples(json_file)
+    labeled_data = [example for example in all_data if hasattr(example, "narrative")]
+    unlabeled_data = [
+        example for example in all_data if not hasattr(example, "narrative")
+    ]
+
+    labeled_train = labeled_data[: int(split * len(labeled_data))]
+    labeled_eval = labeled_data[int(split * len(labeled_data)) :]
+    unlabeled_train = unlabeled_data[: int(split * len(unlabeled_data))]
+    unlabeled_eval = unlabeled_data[int(split * len(unlabeled_data)) :]
+
+    return labeled_train, labeled_eval, unlabeled_train, unlabeled_eval
