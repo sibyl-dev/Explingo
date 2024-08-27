910b0389ebd9a1d3ebcae0a8a5aa7a8e6bb656a2
Ola Zytek <zyteka@mit.edu>
2024-08-27 13:46:34 -0400

    Reorganizing

--- full-experiment/examples.py
+++ full-experiment/examples.py
@@ -2,24 +2,24 @@ import dspy
 import json
 
 
-def create_example(convo):
+def create_example(entry):
     example = dspy.Example(
-        explanation=convo["explanation"],
-        context=convo["context"],
-        explanation_format=convo["explanation_format"],
+        explanation=entry["explanation"],
+        context=entry["context"],
+        explanation_format=entry["explanation_format"],
     )
-    if "description" in convo:
-        example.narrative = convo["description"]
-    if "bad_description" in convo:
-        example.bad_narrative = convo["bad_description"]
+    if "narrative" in entry:
+        example.narrative = entry["narrative"]
+    if "bad_narrative" in entry:
+        example.bad_narrative = entry["bad_narrative"]
     return example.with_inputs("explanation", "context", "explanation_format")
 
 
 def load_examples(json_file):
     training_data = json.load(open(json_file, "r"))
     examples = []
-    for convo in training_data:
-        examples.append(create_example(convo))
+    for entry in training_data:
+        examples.append(create_example(entry))
     return examples
 
 
