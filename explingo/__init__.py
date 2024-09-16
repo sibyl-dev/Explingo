4c7cf7f637061facfb26129ed71bcc2efd44eb13
Ola Zytek <zyteka@mit.edu>
2024-09-16 03:20:55 -0400

    Bumpversion

--- explingo/__init__.py
+++ explingo/__init__.py
@@ -1,4 +1,8 @@
 from narrator import Narrator
 from grader import Grader
 
+__author__ = "MIT Data To AI Lab"
+__email__ = "dailabmit@gmail.com"
+__version__ = "0.1.0dev0"
+
 __all__ = ["Narrator", "Grader"]
