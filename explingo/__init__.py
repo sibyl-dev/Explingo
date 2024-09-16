4d7cde29cde8ff200a845425339701b043829015
Ola Zytek <zyteka@mit.edu>
2024-09-16 03:42:47 -0400

    Updating official explingo version

--- explingo/__init__.py
+++ explingo/__init__.py
@@ -3,6 +3,6 @@ from explingo.grader import Grader
 
 __author__ = "MIT Data To AI Lab"
 __email__ = "dailabmit@gmail.com"
-__version__ = "0.1.0dev2"
+__version__ = "0.1.0.1"
 
 __all__ = ["Narrator", "Grader"]
